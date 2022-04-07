e9b81765b7554d6878fcdb139517ce8a606dc625
Mirza <mirzano05@gmail.com>
2022-04-07 19:58:08 +0300

    Homework 5

---
+++ lesson_5_1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace lesson_5_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = "lesson5.txt";
+            string fileStartup = "startup.txt";
+            DateTime start = DateTime.Now;
+
+            File.WriteAllText(fileName, Console.ReadLine());    //задание 1
+            
+            if (File.Exists(fileStartup))                       //звдвние 2
+            {
+                File.AppendAllText(fileStartup, start.ToString());   
+                File.AppendAllText(fileStartup, Environment.NewLine);
+            }
+            else
+            {
+                File.Create(fileStartup);
+            }
+            
+                      
+
+            Console.WriteLine(File.ReadAllText(fileStartup));
+
+        }
+    }
+}
---
+++ lesson_5_3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace lesson_5_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите числа от 0 до 255");
+            string numbStr = Console.ReadLine();
+            string[] numbStrArr = numbStr.Split();
+
+            byte[] numbArr = new byte[100];
+            for (int i = 0; i < numbStrArr.Length; i++)
+            {
+                numbArr[i] = Convert.ToByte(numbStrArr[i]);
+            }
+
+            string bytes = "bytes.bin";
+            File.WriteAllBytes(bytes, numbArr);
+        }
+    }
+}
