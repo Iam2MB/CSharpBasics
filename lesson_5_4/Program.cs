842eec645ffdf358a0745919a0dfb2b606ed310e
Mirza <mirzano05@gmail.com>
2022-04-11 21:26:43 +0300

    Без рекурсии готово

--- lesson_5_4/Program.cs
+++ lesson_5_4/Program.cs
@@ -1,31 +1,23 @@
 ﻿using System;
 using System.IO;
 
-namespace lesson_5_4
+namespace lesson_5_4 //Сохранить дерево каталогов и файлов по заданному пути в текстовый файл - Без рекурсии
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            string dir = @"D:\Dir";
+            string dir = @"D:\dir";
             string dirСontents = @"D:\dirСontents.txt";
-                        
+           
             if (Directory.Exists(dir))
             {
-                if (File.Exists(dirСontents))
+                File.WriteAllText(dirСontents, string.Empty);
+                string[] content = Directory.GetFileSystemEntries(dir, "*", SearchOption.AllDirectories);
+                foreach (string i in content)
                 {
-                    File.WriteAllText(dirСontents, string.Empty);
-                    string[] content = Directory.GetFileSystemEntries(dir, "*", SearchOption.AllDirectories);
-                    foreach (string i in content)
-                    {
-                        File.AppendAllText(dirСontents, i);
-                        File.AppendAllText(dirСontents, Environment.NewLine);
-                        Console.WriteLine(i);
-                    }
-                }
-                else
-                {
-                    File.Create(dirСontents);
+                    File.AppendAllText(dirСontents, $"{i}\n");
+                    Console.WriteLine(i);
                 }
             }
             else
@@ -33,7 +25,7 @@ namespace lesson_5_4
                 Console.WriteLine("Такого каталога не существует");
             }
             
-            Console.ReadKey();
+            Console.ReadKey(true);
         }
     }
 }
