93e9117b3de5c3682f6a88d1027464d9694369db
Mirza <mirzano05@gmail.com>
2022-04-17 19:42:32 +0300

    Homework_7

---
+++ lesson_7/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace lesson_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = "password";
+            Console.WriteLine("Enter password:");
+            string input = Console.ReadLine();
+
+            while (input == password == false)
+            {
+                Console.WriteLine("Invalid password!\n");
+                Console.WriteLine("Enter password again:");
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine("Welcome!");
+            Console.ReadKey(true);
+        }
+    }
+}
